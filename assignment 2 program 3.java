549250e572eb344d58249e49a6e2be48a778abd3
GS1211 <manu174303@gmail.com>
2018-08-19 10:56:35 +0530

    Create assignment 2 program 3.java
    * pattern program

---
+++ assignment 2 program 3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Pp
+{
+
+public static void main(String[] x)
+
+{
+Scanner i=new Scanner(System.in);
+
+int a;
+
+int b;
+
+System.out.println("input n");
+
+int n;
+n=i.nextInt();
+
+for(a=0;a<n;a++)
+
+{
+
+for(b=0;b<a;b++)
+
+{
+System.out.println("*");
+
+}
+
+
+System.out.println("\n");
+}
+}
+}
